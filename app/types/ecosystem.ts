f9c5412801aa900996dfb167f8d27b9cd7953336
Precious Oritsedere <rukyjacob@gmail.com>
2026-07-16 12:24:56 +0100

    Add facilities fields to ecosystem summary response type.

--- app/types/ecosystem.ts
+++ app/types/ecosystem.ts
@@ -2,6 +2,8 @@ export interface EcosystemSummaryResponse {
   number_of_opportunities: number;
   number_of_publishers: number;
   number_of_activities: number;
+  number_of_facility_types: number;
+  number_of_facilities: number;
   percentage_of_local_authorities: number;
   number_of_activity_providers: number;
   date: string;
